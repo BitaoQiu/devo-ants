544de915d1f97abcde5691a389dc76c81e0930bd
Bitao Qiu <enjoycloud@gmail.com>
2021-11-12 16:26:55 +0100

    Update BPA.R

--- BPA.R
+++ BPA.R
@@ -1,14 +1,15 @@
 # Backward progressive algorithm that predicts caste identities of a target stage based on the principle of developmental continuity. 
 source('shared_functions.R')
 # sampleTable.used is the sample information.
-# Using prediction (validation) of 2nd instar larvae in M. pharaonis as an example.
-train_age = '2nd'
-test_age = '3rd'
+# Using M. pharaonis 2nd instar larvae as an example [validation].
+train_age = '2nd' # Stage with unknown caste identities.
+test_age = '3rd' # Stage with known caste identities.
 
 # This step reads in the gene expression matrix and sample information into a list, exp_data. See shared_functions.R for detail.
 exp_data = quick_subset(c(train_age,test_age),all.sampletable = sampleTable.used,deg_test = F,ab.type = 'count.upper')
 
-# Step 0: Identificaton of caste DEGs at 3rd instar as features (candidate genes). Note: One can use all genes as features, but the signal to noise ratio will be very low.
+# Step 0: Identificaton of caste DEGs at 3rd instar as features (candidate genes). 
+# Note: One can also use whole transcriptome as features, but the signal to noise ratio will be lower.
 exp_deg = quick_subset(test_age,all.sampletable = sampleTable.used,deg_test = T,deg_contrast = 'caste',LRT = F)
 exp_deg$res = results(exp_deg$dds,contrast = c('caste','Gyne','Worker'),alpha = 5e-2,lfcThreshold = 0)
 summary(exp_deg$res)
@@ -20,11 +21,11 @@ train_data = which(exp_data$sampleInfo$age %in% train_age)
 test_data = which(exp_data$sampleInfo$age %in% test_age)
 
 # Code for BPA:
-### Beginning of code:
+
 reverse_pca.exp = function(train_data.exp, test_data.exp, train_age, test_age, sample.info,
                            correct_dev = T, method_d = 'combat',n_gene = 512){
   filter_table = sample.info[c(colnames(train_data.exp),colnames(test_data.exp)),]
-  batch = droplevels(filter_table$age)
+  batch = droplevels(filter_table$age) # We consider age as a co-variate. If correct_dev == T,we normalise the mean expression levels across age.
   modcombat = model.matrix(~1, data= filter_table)
   shared_genes = intersect(rownames(train_data.exp),rownames(test_data.exp))
   shared_genes = rownames(train_data.exp[shared_genes,])[order(apply(train_data.exp[shared_genes,],1,var),decreasing = T)[c(1:n_gene)]]
@@ -50,30 +51,29 @@ reverse_pca.exp = function(train_data.exp, test_data.exp, train_age, test_age, s
         combat_edata[,which(filter_table$age %in% i)] = exp.filter[,which(filter_table$age %in% i)] - rowMeans(exp.filter[,which(filter_table$age %in% i)])}
     }
   }
-  if (correct_dev != T)
+  if (correct_dev != T) # If correct_dev == F, we don't normalize the expression difference.
   {combat_edata = exp.filter}
   trainning_data = combat_edata[,which(colnames(combat_edata) %in% rownames(filter_table)[which(filter_table$age %in% train_age)])]
   trainning_data.centre = trainning_data - rowMeans(trainning_data)
   n = 10
-  tmp_1 = svd(trainning_data.centre,nu = n,nv = n)
+  # Step 1.Construction of PCs from transcriptomes of samples with unknown caste identities.
+  tmp_1 = svd(trainning_data.centre,nu = n,nv = n) 
   testing_data = combat_edata[,which(filter_table$age %in% c(test_age,train_age))]
   testing_data.centre = testing_data - rowMeans(testing_data)
-  pc_data_test = data.frame(t(t(tmp_1$u) %*% testing_data.centre))
+  # Step 2. Projection of transcriptomes of samples with known caste identities onto the constructed PCs.
+  pc_data_test = data.frame(t(t(tmp_1$u) %*% testing_data.centre)) 
   pc_data_test = cbind(pc_data_test, filter_table[rownames(pc_data_test),])
   names(pc_data_test)[c(1:n)] = c(paste0('PC',c(1:n)))
   return(pc_data_test)
 }
-### End of code.
-
-# Step 1: Construction of PCs from transcriptomes of 2nd instar larvae and 
-# Step 2: Projection of the 3rd instar transcriptomes onto the constructed PCs.
+# Step 1 and Step 2.
 pca_data = reverse_pca.exp(exp_data$abundance[candidate_genes,train_data],
                            exp_data$abundance[candidate_genes,test_data],
                            train_age,test_age,
                            sample.info = exp_data$sampleInfo,
                            n_gene = length(candidate_genes))
 
-# Step 3.a: Visually examine the PC value similarity between 2nd instar and 3rd instar larvae. 
+# Step 3.a: Visual examination of the PC value similarity between 2nd instar and 3rd instar larvae. 
 ggplot(pca_data,aes(x = PC1, y = PC2,size = log(body_length), col = caste))+
   geom_point()+
   scale_size_continuous(range = c(.5,3))+
